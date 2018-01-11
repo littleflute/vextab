5d65cff5efe43550bbfd7ff93905a13e54ad1f19
littleflute <13699175164@sina.cn>
2018-01-12 05:00:50 +0800

    Create s0.js

---
+++ tests/songs/s0.js
@@ -0,0 +1,17 @@
+//file: s0.js  by littleflute 2018/1/12 4:15 bjt
+var id_song = 0;
+function _blSongClass(){
+	var v = "s0.js: v0.0.13";
+	var tt = bl$("id_ta_song_"+ id_song);
+	tt.value = new Date + v; 
+	
+					var d = bl$("id_blClassDiv_4_wrap1");
+					var ta = bl$("blah"); 
+					if(!ta.old){ta.old=null;}
+					if(ta.old!=null){ta.old.value = ta.value;}
+					ta.value = tt.value;
+					ta.old=tt; 
+					d.showVextab();
+
+}
+var s0 = new _blSongClass;

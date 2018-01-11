20eac1704fc27ed641c98d8c98457cf7275fc7a2
littleflute <13699175164@sina.cn>
2018-01-12 05:25:43 +0800

    Update s0.js

--- tests/songs/s0.js
+++ tests/songs/s0.js
@@ -1,13 +1,38 @@
 //file: s0.js  by littleflute 2018/1/12 4:15 bjt
 var id_song = 0;
-function _myData(){
-	var r = "\ntabstave";
+
+
+function _myData()
+{
+/*
+_begine_
+	
+options space=20
+tabstave notation=true tablature=false  
+key=A time=4/4
+notes :8 B/4 :q C/5 :8 B/4 :q C/5 B/4 |  :8 B/4 :q C/5 :8 B/4 :qd C/5 :8 E/4 |  F-A-A-A/4 B-G-E/4-C/5 | :h bC/5 :qd bC/5 :8 E/5|
+text :w, .1, A, A,|
+text :h, .1, D, E,| 
+text :w, .1, F#m
+
+_end_
+*/
+}
+function _getData()
+{
+	var r = "";
+	var t = bl$("id_ta_song_"+ id_song);
+	t.value = _myData;
+	var a = t.value;
+	var b = a.split("_begine_");
+	var c = b[1].split("_end_");
+	r = c[0];
 	return r;
 }
 function _blSongClass(){
-	var v = "s0.js: v0.0.14";
+	var v = "s0.js: v0.0.15";
 	var tt = bl$("id_ta_song_"+ id_song);
-	tt.value = _myData(); 
+	tt.value = _getData(); 
 	
 	var d = bl$("id_blClassDiv_4_wrap1");
 	var ta = bl$("blah"); 

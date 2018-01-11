3466b6ed5cda1ce3c7f360bc79cda08b87b3c676
littleflute <13699175164@sina.cn>
2018-01-12 03:27:20 +0800

    Update SongEditClass.js

--- tests/SongEditClass.js
+++ tests/SongEditClass.js
@@ -1,29 +1,7 @@
 
 //file: SongEditClass.js  by littleflute 2018/1/12 2:49 bjt
-function _blEditClass (_o,_d,_t){
-	this.v = "v0.0.34";
-	var o = _o;
-	var d = _d;
-	var ta = _t;
-	var r = blo0.blDiv(o,o.id+"_blMajorChordClass","_blMajorChordClass",blColor[4]);	 
-  	r.C = blo0.blBtn(r,r.id + "C","C",blGrey[3]);  	
-	r.C.onclick = function(){		ta.value += bl$("s1").value + _strMajorChord(1,2); d.showVextab(); 		}
-  	r.D = blo0.blBtn(r,r.id + "D","D",blGrey[3]);  	
-	r.D.onclick = function(){		ta.value += bl$("s1").value + _strMajorChord(3,2); d.showVextab(); 		}
-  	r.E = blo0.blBtn(r,r.id + "E","E",blGrey[3]);
-  	r.E.onclick = function(){		ta.value += bl$("s1").value + _strMajorChord(5,2); d.showVextab(); 		}
-	function _strMajorChord(i,j){
-		var r = "notes :8  " + i + "/" + j + " " + (i+4)+ "/" + j +" " + (i+7) + "/" + j + " " + (i+12) + "/" + j + " " + (i+7) +"/" + j + " |";
-		return r;
-	}
-	var r = blo0.blDiv(o,o.id+"_add_line","[+line]",blColor[4]);	 
-  	r.a0 = blo0.blBtn(r,r.id + "a0","a0",blGrey[0]);r.a0.onclick = function(){	ta.value += "\ntabstave"; d.showVextab(); }
-  	r.a1 = blo0.blBtn(r,r.id + "a1","a1",blGrey[0]);r.a1.onclick = function(){ta.value += "\ntabstave notation=true clef=bass key=Ab time=C| \n notes 4-5/6"; d.showVextab(); }
-  	r.a2 = blo0.blBtn(r,r.id + "a2","a2",blGrey[0]);r.a2.onclick = function(){ta.value += "\ntabstave notation=true tablature=false \n notes Cn-D-E/4 F#/5"; d.showVextab(); }
-	r.a3 = blo0.blBtn(r,r.id + "a3","a3",blGrey[0]);r.a3.onclick = function(){ta.value += "\ntabstave notation=true \n notes 4-5-6/3 10/4"; d.showVextab(); }
-} 
 function _blSongEditClass (_d,_t){
-	this.v = "v0.0.45";
+	this.v = "v0.0.52";
 	var _v = null;
 	var d = _d;
 	var ta = _t;
@@ -44,12 +22,12 @@ function _blSongEditClass (_d,_t){
 					var html = "ns" + (ns.length + 1);
 					var b = blo0.blBtn(div.v.tb,id,html,blColor[1]);
 					b.ta = blo0.blTextarea(document.body,b.id+"_ta","xd",0,0,100,100,blColor[0]);
-					b.onclick = function(tt){
-						var n = 0;
-						return function(){
-							n++;
-							tt.value = n;
-						}
+					b.onclick = function(tt){return function(){
+						if(!ta.old){ta.old=null;}
+						if(ta.old!=null){ta.old.value = ta.value;}
+						ta.value = tt.value;
+						ta.old=tt; 
+						d.showVextab();}
 					}(b.ta);
 					ns.push(b);
 				}
@@ -78,3 +56,25 @@ function _blSongEditClass (_d,_t){
 		_on_off_div(btn,div);	
 	}; 
 }
+function _blEditClass (_o,_d,_t){
+	this.v = "v0.0.34";
+	var o = _o;
+	var d = _d;
+	var ta = _t;
+	var r = blo0.blDiv(o,o.id+"_blMajorChordClass","_blMajorChordClass",blColor[4]);	 
+  	r.C = blo0.blBtn(r,r.id + "C","C",blGrey[3]);  	
+	r.C.onclick = function(){		ta.value += bl$("s1").value + _strMajorChord(1,2); d.showVextab(); 		}
+  	r.D = blo0.blBtn(r,r.id + "D","D",blGrey[3]);  	
+	r.D.onclick = function(){		ta.value += bl$("s1").value + _strMajorChord(3,2); d.showVextab(); 		}
+  	r.E = blo0.blBtn(r,r.id + "E","E",blGrey[3]);
+  	r.E.onclick = function(){		ta.value += bl$("s1").value + _strMajorChord(5,2); d.showVextab(); 		}
+	function _strMajorChord(i,j){
+		var r = "notes :8  " + i + "/" + j + " " + (i+4)+ "/" + j +" " + (i+7) + "/" + j + " " + (i+12) + "/" + j + " " + (i+7) +"/" + j + " |";
+		return r;
+	}
+	var r = blo0.blDiv(o,o.id+"_add_line","[+line]",blColor[4]);	 
+  	r.a0 = blo0.blBtn(r,r.id + "a0","a0",blGrey[0]);r.a0.onclick = function(){	ta.value += "\ntabstave"; d.showVextab(); }
+  	r.a1 = blo0.blBtn(r,r.id + "a1","a1",blGrey[0]);r.a1.onclick = function(){ta.value += "\ntabstave notation=true clef=bass key=Ab time=C| \n notes 4-5/6"; d.showVextab(); }
+  	r.a2 = blo0.blBtn(r,r.id + "a2","a2",blGrey[0]);r.a2.onclick = function(){ta.value += "\ntabstave notation=true tablature=false \n notes Cn-D-E/4 F#/5"; d.showVextab(); }
+	r.a3 = blo0.blBtn(r,r.id + "a3","a3",blGrey[0]);r.a3.onclick = function(){ta.value += "\ntabstave notation=true \n notes 4-5-6/3 10/4"; d.showVextab(); }
+} 

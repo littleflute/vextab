14e872ddfb2dc99bd5feb8e345f38bf633b2a4c8
littleflute <13699175164@sina.cn>
2018-03-09 00:54:23 -0800

    Update s1.js

--- tests/songs/s1.js
+++ tests/songs/s1.js
@@ -1,13 +1,53 @@
-//file: s1.js  by littleflute 2018/1/12 5:11 bjt
+//file: s1.js  by littleflute 2018/3/9 0:54 usa
 var id_song = 1;
-function _myData(){
-	var r = "\ntabstave notation=true clef=bass key=Ab time=C| \n notes 4-5/6";
+
+
+function _myData()
+{
+/*
+_begine_
+	
+
+    
+options space=20
+tabstave notation=true tablature=false  
+key=A time=4/4
+notes :8 B/4 :q C/5 :8 B/4 :q C/5 B/4 |  :8 B/4 :q C/5 :8 B/4 :qd C/5 :8 E/4 |  F-A-A-A/4 B-G-E/4-C/5 | :h bC/5 :qd bC/5 :8 E/5|
+text :w, .1, A, A,|
+text :h, .1, D, E,| 
+text :w, .1, F#m
+
+options space=20
+tabstave notation=true tablature=false
+
+key=A time=4/4
+notes :8 F-E/5 :q F/5 :8 D-C/5-B-A/4 |:h bA/4 E/4 |:q A/4 :8 C/5 B/4 bB/4 E-F-E/4 |:q A/4
+text :h, .1, D, E,| 
+text :w, .1, A
+text :w, .1, A  
+
+options space=25
+
+
+
+_end_
+*/
+}
+function _getData()
+{
+	var r = "";
+	var t = bl$("id_ta_song_"+ id_song);
+	t.value = _myData;
+	var a = t.value;
+	var b = a.split("_begine_");
+	var c = b[1].split("_end_");
+	r = c[0];
 	return r;
 }
 function _blSongClass(){
 	var v = "s0.js: v0.0.16";
 	var tt = bl$("id_ta_song_"+ id_song);
-	tt.value = _myData(); 
+	tt.value = _getData(); 
 	
 	var d = bl$("id_blClassDiv_4_wrap1");
 	var ta = bl$("blah"); 
@@ -17,4 +57,4 @@ function _blSongClass(){
 	ta.old=tt; 
 	d.showVextab();
 }
-var s = new _blSongClass;
+var s0 = new _blSongClass;

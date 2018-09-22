e2b1e3a5fbddb54d9ecfc5d307529493933d2310
littleflute <13699175164@sina.cn>
2018-09-21 17:38:08 -0700

    Update s3.js

--- tests/songs/s3.js
+++ tests/songs/s3.js
@@ -1,4 +1,4 @@
-//file: s3.js  by littleflute 2018/3/9 0:59 usa
+//file: s3.js  by littleflute 2018/9/21 17:379  oregon,usa
 var id_song = 3;
 
 
@@ -7,17 +7,26 @@ function _myData()
 /*
 _begine_
 	
-var md = blo0.blMDiv(document.body,"id_mdiv_music", "md-v0.0.2",   300,100,500,400,blColor[1]);
-var v1 = blo0.blDiv(md,md.id+"v1","v1",blGrey[0]);   
-var b1 = blo0.blBtn(v1,"v1b1","b1a",blGrey[1]);
+var md = blo0.blMDiv(document.body,"id_mdiv_music", "md-v0.0.4",   300,100,500,400,blColor[1]);
+var v1 = blo0.blDiv(md,md.id+"v1","v1:",blGrey[0]);   
+var v2 = blo0.blDiv(md,md.id+"v2","v2:",blGrey[1]);   
+v2.bs = [];
+var b1 = blo0.blBtn(v1,"v1b1","reflash",blGrey[1]);
 b1.onclick = function(){
     var t1 = bl$("blah");
     var d1 = bl$("id_blClassDiv_4_wrap1");
     t1.value = "221a5";
     d1.showVextab ();
 }
+var b2 = blo0.blBtn(v1,"v1b2","note+",blGrey[2]);
+ 
+b2.onclick = function(){   
+    var _btn = blo0.blBtn(v2, v2.id + v2.bs.length,v2.bs.length,blGrey[v2.bs.length]);
+    v2.bs.push(_btn);
+}
 
 _on_off_div(null,md);
+
 
 _end_
 */

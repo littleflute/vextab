e91507358b8fc537474b8e015ca07d483d8101fc
littleflute <13699175164@sina.cn>
2018-10-17 02:06:22 -0700

    Update s5.js

--- tests/songs/s5.js
+++ tests/songs/s5.js
@@ -7,18 +7,12 @@ function _myData()
 {
 /*
 _begine_
-	options space=50 font-size=22
-
-tabstave notation=true tablature=false   time=4/4 
-key=F   tuning= E/5,B/4,g/4,D/4,A/3,E/3
-  notes  :qd 8/1 :8 8/1 :q 8/1 :8 5/1-8/2| :hd 6/2 :8 8/2-5/1| :qd 6/1 :8 5/1 :q 6/1 :8 7/3 6/2| :hd 5/2 :8 5-5/3| 
-  text :w, F, Dm,|,bB,|,C, |
-  text ++, .-4, :w, a
-
-options space=15
-
-options width=750 space = 50 
-
+	options stave-distance=30 space=10
+tabstave notation=true  tablature=false key=A time=4/4
+  voice
+    notes :q (5/2.5/3.7/4) :8 7p5h6/3 ^3^ 5h6h7/5 ^3^ :q 7V/4
+  voice
+    notes :h 5/6 :q  5/6 :8 3-5/6
 
 
 

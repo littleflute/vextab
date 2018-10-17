d6c9b87420bf0c3d320bf7bd9c3265d526c3f8fd
littleflute <13699175164@sina.cn>
2018-10-16 21:18:48 -0700

    Update s4.js

--- tests/songs/s4.js
+++ tests/songs/s4.js
@@ -9,14 +9,18 @@ function _myData()
 _begine_
 	
 	
-options space=50 font-size=22
+	
+	
+options space=50 font-size=14 width = 666
 
 tabstave notation=true tablature=false   time=4/4 
 key=F   tuning= E/5,B/4,g/4,D/4,A/3,E/3
-  notes :8 5-5/3 | :qd 5/1 :8 5/1 :q 5/1 :8 6-5/2| :hd 7/3 :8 7-7/3| :qd 6/1 :8 5/1 :q 6/1 :8 7/3 6/2| :hd 5/2 :8 5-6/1| 
+  notes :8 5-5/3 | :qd 5/1  :8 5/1 :q 5/1 :8 6-5/2  | :hd 7/3 :8 7-7/3| :qd 6/1 :8 5/1 :q 6/1 :8 7/3 6/2| :hd 5/2 :8 5-6/1|
   text :q,.1, ,| ,:w, F, Dm,|,bB,|,C, |
   text ++, .-4, :w, 因着十架爱
-  text ++, .11, :w, -1- --------------------------------
+ 
+  text ++, .10, :8, 主,你,| ,:qd ,的,:8,身,:q,体, :8,为,我,|,:hd ,舍 ,:8,你 , 的,|,:qd,宝,:8,血,:q,为,:8,我,流,|,:hd,出,:8,诚,然
+  text ++, .12, :w, -1- --------------------------------
 
 options space=55
 
@@ -46,10 +50,45 @@ key=F   tuning= E/5,B/4,g/4,D/4,A/3,E/3
   notes :q 6/1 :8 b 6/1 7/3 5/2 5/2 6/2 8/2| :hd 6/2 :8 6/2-8/2| 
   text :w, F, Dm,|,:h,bB,C, |, :w,F
   text ++, .11, :w, -4- --------------------------------
+	options space=50 font-size=22
 
+tabstave notation=true tablature=false   time=4/4 
+key=F   tuning= E/5,B/4,g/4,D/4,A/3,E/3
+  notes  :hd 5/1  :q  8/1| :hd 8/1 :8  5/1-8/2|
+  notes  :hd 6/2  :8 3b5/1| :hd 5/1 :8  5/1 8/2|
+  text :w, F, C,|,Dm,|,bB, |
+  text ++, .11, :w, -5- --------------------------------------
 
-	options space=50 font-size=22 
+	options space=50 font-size=22
+
+tabstave notation=true tablature=false   time=4/4 
+key=F   tuning= E/5,B/4,g/4,D/4,A/3,E/3
+  notes  :hd 6/2  :8  10b8/1| :hd 8/1 :8  5/1-8/2|
+  notes  :hd 6/2  :8 5b3/1| :hd 8/2 :8  6/2 8/2|
+  text :w, bB, Am,|,bB,|,C, |
+  text ++, .11, :w, -6- --------------------------------------
+
+	options space=50 font-size=22
+
+tabstave notation=true tablature=false   time=4/4 
+key=F   tuning= E/5,B/4,g/4,D/4,A/3,E/3
+  notes  :hd 5/1  :q  8/1| :hd 8/1 :8  5/1-8/2|
+  notes  :hd 6/2  :8 3b5/1| :hd 5/1 :8  5/1 8/2|
+  text :w, F, C,|,Dm,|,bB, |
+  text ++, .11, :w, -7- --------------------------------------
+
+	options space=50 font-size=22
+
+tabstave notation=true tablature=false   time=4/4 
+key=F   tuning= E/5,B/4,g/4,D/4,A/3,E/3
+  notes  :hd 6/2  :8  10b8/1| :hd 8/1 :8  5/1-8/2|
+  notes  :hd 6/2  :8 5b3/1| :hd 8/2 :8  6/2 8/2|
+  text :w, bB, Am,|,:h,bB,C, |,:w ,F,|
+  text ++, .11, :w, -8- --------------------------------------
+
+options space=15
  
+
 
 
 _end_

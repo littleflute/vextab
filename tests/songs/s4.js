57ef670925fe0595e56e2a6fef507e000335b95e
littleflute <13699175164@sina.cn>
2018-10-19 12:40:14 -0700

    Update s4.js

--- tests/songs/s4.js
+++ tests/songs/s4.js
@@ -9,11 +9,14 @@ function _myData()
 _begine_
 	
 	
-	options space=50 font-size=14 width = 666
+options space=50 font-size=14 width = 800
 
 tabstave notation=true tablature=false   time=4/4 
 key=F   tuning= E/5,B/4,g/3,D/3,A/2,E/2
+voice
   notes :8 (6/1.8/2)(8/1.10/2)|:q (10/1.11/2):8 (1/1.3/2)(1/1.3/2) (3/1.5/2)(3/1.5/2) (5/1.6/2)(6/1.8/2) |:w (8/1.10/2.10/3)|(10/1.10/2.7/3)|(10/1.11/2.8/4)|:hd (8/1.8/2.5/3) :8 5-5/3
+voice
+
   text :q,.-1, ,| ,:h, bB, C,|,:w,F,|,Dm,|,bB,|,C
   text ++, .-5, :8,|, :w, ,因着十架爱
  
@@ -22,7 +25,7 @@ key=F   tuning= E/5,B/4,g/3,D/3,A/2,E/2
 
 	
 	
-options space=70 font-size=14 width = 666
+options space=70 font-size=14 width = 800
 
 tabstave notation=true tablature=false   time=4/4 
 
@@ -52,70 +55,104 @@ voice
 options space=55
 
 
-options space=50 font-size=14
+options space=50 font-size=14 width = 800
 
 tabstave notation=true tablature=false   time=4/4 
 key=F   tuning= E/4,B/3,g/3,D/3,A/2,E/2
+voice 
+ notes :h 20/1 :8 ## 5-5-5/1|:hd 1/1 :8 1-1/1|:q 10/1 :8 b10-8-10-1-1-5/1 |:hd 3/1 :8 8-10/1
+voice 
   notes  :q 5/1  :8 b 5/1 :8 5/1 :8 5/1 6/2 :8 6-5/2| :hd 7/3 :8  7-7/3|
   notes  :q 6/1 :8 b 6/1 5/1 6/1 7/3 7/3 6/2| :hd 5/2 :8 5-6/1| 
   text :w, F, Dm,|,bB,|,C, |
-  text ++, .10, :qd ,受,:8,鞭,伤,我,得,医,|,:hd ,治, :8, 因,你,|,:qd,受,:8,刑,罚,我,得,平,|,:hd,安,:8,主,啊 
-  text ++, .12, :w, -3- --------------------------------------
+  text ++, .13, :qd ,受,:8,鞭,伤,我,得,医,|,:hd ,治, :8, 因,你,|,:qd,受,:8,刑,罚,我,得,平,|,:hd,安,:8,主,啊 
+  text ++, .15, :w, -3- --------------------------------------
 
+options space=55
 options space=15
 
 
-	options space=50 font-size=14
+
+options space=50 font-size=14 width = 800
 tabstave notation=true tablature=false   time=4/4 
 key=F   tuning= E/4,B/3,g/3,D/3,A/2,E/2
+voice 
+  notes :q 13/1 :8 b13-13-13-8-8-6/1|:hd 5/1 :8 6-8/1|
+  notes :q 10/1 :8 10-1-3-3-5-6/1|:hd 5/1 :8 5-6/1
+voice 
   notes  :q 8/1 :8 b 8/1 8/1  8/1 5/1 5/1-8/2| :hd 6/2 :8 8/2-5/1|
   notes  :q 6/1 :8 b 6/1 7/3 5/2 5/2 6/2 8/2| :hd 6/2 :8 6/2-8/2| 
   text :w, F, Dm,|,:h,bB,C, |, :w,F  
-text ++, .10, :qd ,我,:8,回,转,到,你,面,|,:hd,前,:8,我,要,|,:qd,经,:8,历,医,治,和,平,|,:hd,安,:8,医,治
-  text ++, .12, :w, -4- --------------------------------
-	options space=50 font-size=14
+text ++, .13, :qd ,我,:8,回,转,到,你,面,|,:hd,前,:8,我,要,|,:qd,经,:8,历,医,治,和,平,|,:hd,安,:8,医,治
+  text ++, .15, :w, -4- --------------------------------
 
+options space=80 font-size=14
 tabstave notation=true tablature=false   time=4/4 
 key=F   tuning= E/4,B/3,g/3,D/3,A/2,E/2
+voice
+  notes :hd 8/1 :q 13/1| :hd 12/1 :8 8/1-6/1|
+  notes :hd 5/1 :8 6b8/1|:hd 8/1 :8 13-12/1
+voice
   notes  :hd 5/1  :q  8/1| :hd 8/1 :8  5/1-8/2|
   notes  :hd 6/2  :8 3b5/1| :hd 5/1 :8  5/1 8/2|
-  text :w, F, C,|,Dm,|,bB, |
-  text ++, .10, :hd ,和,:q,平,|,:hd,安,:8,因,着,|,:hd,十,:q,架,|,:hd,爱,:8,众,罪
-  text ++, .12, :w, -5- --------------------------------------
+  text :w, F, C,|,Dm,|,Am, |
+  text ++, .12, :hd ,和,:q,平,|,:hd,安,:8,因,着,|,:hd,十,:q,架,|,:hd,爱,:8,众,罪
+  text ++, .15, :w, -5- --------------------------------------
 
-	options space=50 font-size=14
+	
+options space=80 font-size=14
+	 
+
+
 
 tabstave notation=true tablature=false   time=4/4 
 key=F   tuning= E/4,B/3,g/3,D/3,A/2,E/2
+voice
+ notes :hd 10/1 :8 13b12/1| :hd 12/1 :8 13-12/1|
+ notes :hd 10/1 :8 13b12/1| :hd 12/1 :8 5-6/1
+voice
   notes  :hd 6/2  :8  10b8/1| :hd 8/1 :8  5/1-8/2|
   notes  :hd 6/2  :8 5b3/1| :hd 8/2 :8  6/2 8/2|
   text :w, bB, Am,|,bB,|,C, |
-  text ++, .10, :hd ,得,:q,赦,|,:hd,免,:8,因,着,|,:hd,十,:q,架,|,:hd,爱,:8,因,着
-  text ++, .12, :w, -6- --------------------------------------
+  text ++, .12, :hd ,得,:q,赦,|,:hd,免,:8,因,着,|,:hd,十,:q,架,|,:hd,爱,:8,因,着
+  text ++, .14, :w, -6- --------------------------------------
 
-	options space=50 font-size=14
+	
+options space=80 font-size=14
 
 tabstave notation=true tablature=false   time=4/4 
 key=F   tuning= E/4,B/3,g/3,D/3,A/2,E/2
+ voice
+  notes :hd 8/1 :q 13/1| :hd 12/1 :8 8/1-6/1|
+  notes :hd 5/1 :8 6b8/1|:hd 8/1 :8 13-12/1
+voice
   notes  :hd 5/1  :q  8/1| :hd 8/1 :8  5/1-8/2|
   notes  :hd 6/2  :8 3b5/1| :hd 5/1 :8  5/1 8/2|
-  text :w, F, C,|,Dm,|,bB, |
-  text ++, .10, :hd,十,:q,架,|,:hd,爱,:8,因,着,|,:hd,十,:q,架,|,:hd,爱,:8,今,我
-  text ++, .12, :w, **【7】- --------------------------------------
+  text :w, F, C,|,Dm,|,Am, |
+  text ++, .12, :hd,十,:q,架,|,:hd,爱,:8,因,着,|,:hd,十,:q,架,|,:hd,爱,:8,今,我
+  text ++, .14, :w, **【7】- --------------------------------------
 
-	options space=50 font-size=14
-
+	options space=80 font-size=14
+options space=50 font-size=14 width = 800
 tabstave notation=true tablature=false   time=4/4 
 key=F   tuning= E/4,B/3,g/3,D/3,A/2,E/2
+voice
+ notes :hd 5/1 :8 13b12/1|:hd 12/1 :8 13-12/1| :q 10b13b8/1 :8 6b5/1|
+ notes :hd 5/1 :8 18-20/1 =:|
+ notes :hd 5/1 :8 5-6/1 =||
+ notes :hd 5/1 :q 8/1|:w 8/1 =|=
+voice
   notes  :hd 6/2  :8  10b8/1| :hd 8/1 :8  5/1-8/2|
-  notes  :hd 6/2  :8 8b6/2| :hd 6/2 |
+  notes  :hd 6/2  :8 8b6/2| :hd 6/2 :q ## |:hd 6/2 :8 6-8/2 |
+  notes  :hd 6/2  :q 8/2|:w 5/1
   text :w, bB, Am,|,:h,bB,C, |,:w ,F,|
-text ++, .10, :hd,得,:q,洁,|,:hd,净, :8,因,着,|,:hd,十,:q,架,|,:hd,爱
-  text ++, .12, :w, -8- --------------------------------------
+text ++, .12, :hd,得,:q,洁,|,:hd,净, :8,因,着,|,:hd,十,:q,架,|,:hd,爱,:q,a,|,:hd,爱,:8,因,着,|,:hd,爱,:q,阿,|,:w,门
+
+  text ++, .14, :w, -8- --------------------------------------
 
 options space=111
    
- 
+
 
    
  

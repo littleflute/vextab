f410b44fa4285f43e0e25d55c5d5b95a54356176
littleflute <13699175164@sina.cn>
2018-12-28 12:09:48 -0800

    Update s6.js

--- tests/songs/s6.js
+++ tests/songs/s6.js
@@ -7,13 +7,56 @@ function _myData()
 {
 /*
 _begine_
-	options stave-distance=30 space=10
-tabstave notation=true  tablature=false key=A time=4/4
-  voice
-    notes :q (5/2.5/3.7/4) :8 7p5h6/3 ^3^ 5h6h7/5 ^3^ :q 7V/4
-  voice
-    notes :h 5/6 :q  5/6 :8 3-5/6
 
+options space=158 font-size=12 width = 800 scale = 1.012
+tabstave notation=true tablature=false time=4/4
+key=C tuning=E/5,B/4,G/4,D/4,A/3,E/3 
+voice
+notes :8  15/5 12/4 14/4 15/4   12/3 14/3 12/2 13/2 |
+notes :8  12/2 14/3 12/3 15/4 14/4 12/4 15/5 14/5 
+voice
+notes :w 3/5| 3/6 
+
+text .-5 :q,C Major scale
+text ++, .7 ,:8,1,2,.6,3,1,.5,2,3,.4,4,1,|,4,3,.5,2,1,.6,3,2,.7,1,4
+
+options space=33 font-size=12 width = 800 scale = 1.012
+tabstave notation=true tablature=false time=4/4
+key=C tuning=E/5,B/4,G/4,D/4,A/3,E/3 
+voice
+notes :8   12/5 14/5 15/5 12/4 14/4 15/4   12/3 14/3|
+notes :8   12/3 15/4 14/4 12/4 15/5 14/5 12/5 15/6
+voice
+notes :w 0/5| 2/4
+
+text  :8 
+text ++, .8 ,:8,3,4,.7,1,2,.6,3,1,.5,2,3,|,2,1,.6,3,2,.7,1,4,.8,3,2
+
+options space=33 font-size=12 width = 800 scale = 1.012
+tabstave notation=true tablature=false time=4/4
+key=C tuning=E/5,B/4,G/4,D/4,A/3,E/3 
+voice
+notes :8   12/5 14/5 15/5 12/4 14/4 15/4   12/3 14/3|
+notes :8   12/3 15/4 14/4 12/4 15/5 14/5 12/5 15/6
+voice
+notes :w 3/4| 3/5
+
+text  :q
+text ++, .4 ,:8, 3,4,.7,1,2,.6,3,1,.5,2,3,|,2,1,.6,3,2,.7,1,4,.8,3,2
+
+options space=33 font-size=12 width = 800 scale = 1.012
+tabstave notation=true tablature=false time=4/4
+key=C tuning=E/5,B/4,G/4,D/4,A/3,E/3 
+voice
+notes :8   12/5 14/5 15/5 12/4 14/4 15/4   12/3 14/3|
+notes :8   12/2 13/2 12/2 14/3 12/3 15/4 14/4 12/4 
+voice
+notes :w 0/4| 3/6
+
+text  :q
+text ++, .4 ,:8, 3,4,.7,1,2,.6,3,1,.5,2,3,|,4,5,.6,4,3,.7,2,1,.8,3,2
+
+options space=33 
 
 
 

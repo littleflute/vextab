798393f04c5783cc1da84618d6f1bd7fcf3a51c4
littleflute <littleflute@163.com>
2019-08-14 23:44:19 -0700

    xd1.html plx.js*

--- doc/jsPlx/plx.js
+++ doc/jsPlx/plx.js
@@ -1,12 +1,14 @@
 //plx.js
 var vp 	= btnJsPlx.parentElement;
-vp.d1 	= blo0.blDiv(vp, vp.id + "d1","d1-v0.0.14 ::",blGrey[0]);
-vp.md1 	= blo0.blMDiv(vp, vp.id + "md1","md1-",50,50,500,400,blGrey[0]);
+vp.d1 	= blo0.blDiv(vp, vp.id + "d1","d1-v0.0.21 ::",blGrey[0]);
+vp.md1 	= blo0.blMDiv(vp, vp.id + "md1","md1-",850,50,300,400,blGrey[0]);
  
 
 
 
 btnJsPlx.onclick = function(){
 	_on_off_div(this,vp.md1);
+	var b = this;	
+	b.style.background = b.style.background=="red"?blGrey[5]:blColor[4];
 } 
-_on_off_div(btnJsPlx,vp.md1);
+_on_off_div(btnJsPlx,vp.md1); 	btnJsPlx.style.background = btnJsPlx.style.background=="red"?blGrey[5]:blColor[4];
